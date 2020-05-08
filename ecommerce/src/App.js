58172db822e48ebd64702a62a174962c8a7c222d
Henry Neal <henry.neal0@gmail.com>
2020-05-08 13:35:01 -0400

    built out skeleton for auth signin form. adding styling and functionality next

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Auth from "./pages/auth/auth.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={Auth} />
 			</Switch>
 		</div>
 	);
