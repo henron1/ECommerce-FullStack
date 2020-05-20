52accedcfc6633f7d26bc81ae6bb075787ba75eb
Henry Neal <henry.neal0@gmail.com>
2020-05-20 12:46:45 -0400

    built out custom form component and added shrink animation to label when input is active

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // using class component because we need to store what the user is typing in
 
 import "./sign-in.styles.scss";
+import FormInput from '../form-input/form-input.component'
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -29,22 +30,23 @@ class SignIn extends React.Component {
 				<span>Sign in with your email and password</span>
 
 				<form onSubmit={this.handleSubmit}>
-					<input
+					<FormInput
 						name="email"
 						type="email"
 						value={this.state.email}
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
+						label="Email"
 						required
 					/>
-					<label>Email</label>
-					<input
+
+					<FormInput
 						name="password"
 						type="password"
 						value={this.state.password}
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
+						label="Password"
 						required
 					/>
-					<label>Password</label>
 
 					<input type="submit" value="Submit Form" />
 				</form>
