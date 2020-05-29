69d0c7a4131574ccc10550e3a9e8a39b1fa37666
Henry Neal <henry.neal0@gmail.com>
2020-05-29 12:16:37 -0400

    added session persistance, working on signout

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -6,18 +6,45 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Auth from "./pages/auth/auth.component";
 import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={Auth} />
-			</Switch>
-		</div>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			currentUser: null,
+		};
+	}
+
+	// We don't want to remount our app, we just want to always know when Firebase says that auth state has changed.
+	// Firebase makes that happen really easily
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		//subscriber to persist user session
+		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+			console.log(user);
+		});
+	}
+
+	// close sub and prevent memory leaks
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={Auth} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
