9a2539d7b1ba4858737e56a8dc93fe0beea9b208
Henry Neal <henry.neal0@gmail.com>
2020-06-01 13:30:28 -0400

    updates signin, introduced firebase auth persistance

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -24,7 +24,6 @@ class App extends React.Component {
 		//subscriber to persist user session
 		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
 			this.setState({ currentUser: user });
-			console.log(user);
 		});
 	}
 
@@ -36,7 +35,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header />
+				<Header currentUser={this.state.currentUser} />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
--- ecommerce/src/components/custom-button/custom-button.component.jsx
+++ ecommerce/src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import './custom-button.styles.scss'
+import React from "react";
+import "./custom-button.styles.scss";
 // pull children off props that are passed into button and then destructure other props and spread that into our buton.
-const CustomButton = ({ children, ...otherProps }) => (
-    <button className='custom-button' {...otherProps} type="submit">
-        {children}
-    </button>
-)
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+	<button
+		className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+		{...otherProps}
+		type="submit"
+	>
+		{children}
+	</button>
+);
 
 export default CustomButton;
